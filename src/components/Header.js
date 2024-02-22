36c87121e1f42d7b6f65786506d2d67d475ea3a5
pookey1104 <pookey1104@naver.com>
2024-02-23 03:48:52 +0900

    [FEAT] 로그인 후 헤더, 마이페이지 조회 수정 API 연동 #서윤

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/Header.css";
+import styled from "styled-components";
 
 import Coy from "../img/Coy.png";
 import search from "../img/search.png";
@@ -9,14 +10,66 @@ import defalut from "../img/NonProfile.png";
 
 const API_BASE_URL = "https://likelion-running.store/api";
 
+const DropdownContainer = styled.div`
+  z-index: 2;
+  position: absolute;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  width: 145px;
+  top: 125%;
+  right: 0;
+  display: ${(props) => (props.isVisible ? "flex" : "none")};
+  flex-direction: column;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0.1, 0.1, 0.1, 0.2);
+`;
+
+const DropdownItem = styled.div`
+  height: 45px;
+  color: #353e4f;
+  font-family: "Pretendard-Regular";
+  font-size: 15px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  &:hover {
+    color: #439aff;
+  }
+  > img {
+    width: 24px;
+    height: 24px;
+    margin-right: 5px;
+    margin-left: 15px;
+  }
+  &:first-child {
+    border-radius: 10px 10px 0 0;
+  }
+  &:last-child {
+    border-radius: 0px 0px 10px 10px;
+  }
+
+  /* 로그인되지 않은 상태일 때 모든 가장자리의 radius를 추가 */
+  &:only-child {
+    border-radius: 10px;
+  }
+`;
+
 function Header() {
+  const navigate = useNavigate();
+  const dropdownRef = useRef(null);
+
+  const handleNavLinkClick = (path) => {
+    navigate(path);
+  };
+
   const [searchData, setSearchData] = useState("");
 
   const handleInputChange = (e) => {
     setSearchData(e.target.value);
   };
-
-  const navigate = useNavigate();
 
   const handleMainLink = () => {
     navigate("/");
@@ -67,6 +120,42 @@ function Header() {
     }
   }, []);
 
+  const handleLogout = ({ onClose }) => {
+    // 로컬 스토리지에서 토큰 삭제
+    localStorage.removeItem("token");
+    localStorage.removeItem("memberId");
+
+    // 로그아웃 후 /home으로 이동
+    navigate("/");
+
+    // 강제로 페이지 새로고침
+    window.location.reload();
+
+    // 모달 닫기
+    onClose();
+  };
+
+  // 드롭다운 관련 코드
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const handleMenuClick = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownVisible(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="Header">
       <div className="header-left">
@@ -85,8 +174,20 @@ function Header() {
         </div>
         {isLoggedIn ? (
           <div className="login-user">
-            <img className="img-bell" src={bell} alt="알람창" />
-            <img className="img-profile" src={defalut} alt="내 프로필" />
+            <div className="img-bell">
+              <img src={bell} alt="알람창" style={{ cursor: "pointer" }} />
+            </div>
+            <div className="img-profile" onClick={handleMenuClick} style={{ position: "relative" }} ref={dropdownRef}>
+              <img src={defalut} alt="내 프로필" style={{ cursor: "pointer" }} />
+              {isDropdownVisible && (
+                <DropdownContainer isVisible={isDropdownVisible}>
+                  <DropdownItem onClick={() => handleNavLinkClick("/mypage")}>내 프로필</DropdownItem>
+                  <DropdownItem onClick={() => handleNavLinkClick("/like/project")}>내 좋아요</DropdownItem>
+                  <div className="header-line" />
+                  <DropdownItem onClick={handleLogout}>로그아웃</DropdownItem>
+                </DropdownContainer>
+              )}
+            </div>
           </div>
         ) : (
           <div className="user-bar">
--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,14 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Card_Profile_Project from "../components/Card_Profile_Project";
+import { IoChevronDownOutline, IoChevronUpOutline, IoChevronForward } from "react-icons/io5";
 
 import "../styles/MyPage.css";
 import Upload from "../img/upload.png";
 import Default from "../img/NonProfile.png";
 
 function MyPage() {
+  const navigate = useNavigate();
   const [imagePreview, setImagePreview] = useState(null);
+  const [name, setUserName] = useState("");
+  const [job, setJob] = useState("");
+  const [selectedSubJob, setSelectedSubJob] = useState("");
+  const [isJobDropdownOpen, setIsJobDropdownOpen] = useState(false);
+  const [isSubJobDropdownOpen, setIsSubJobDropdownOpen] = useState(false);
+  const [showStackSelection, setShowStackSelection] = useState(false);
+  const [selectedStacks, setSelectedStacks] = useState([]);
+  const [introduce, setIntroduce] = useState("");
+  const [link, setLink] = useState("");
+  const [projects, setProjects] = useState([]);
+  const [memberId, setMemberId] = useState("");
+
+  const API_BASE_URL = "https://likelion-running.store/api";
+
+  useEffect(() => {
+    // useEffect를 사용하여 컴포넌트가 마운트될 때 API 호출
+    const token = localStorage.getItem("token"); // 로컬 스토리지에서 토큰 가져오기
+    const storedMemberId = localStorage.getItem("memberId"); // 로컬 스토리지에서 memberId 가져오기
+    setMemberId(storedMemberId);
+
+    // API 호출 및 데이터 수신
+    axios
+      .get(`${API_BASE_URL}/information/check/my`, {
+        headers: {
+          Authorization: `Bearer ${token}`, // 헤더에 토큰 추가
+        },
+      })
+      .then((response) => {
+        // API 응답 데이터를 받아와서 상태 변수에 설정
+        const userData = response.data;
+        setUserName(userData.name);
+        setIntroduce(userData.introduce);
+        setJob(userData.job);
+        setSelectedSubJob(userData.specificDuty);
+        setSelectedStacks(userData.technics);
+        setLink(userData.link);
+        setProjects(userData.boards);
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+        // 오류 처리
+      });
+  }, []);
 
   const setImageFn = (event) => {
     const file = event.target.files[0];
@@ -21,7 +68,134 @@ function MyPage() {
     }
   };
 
-  const projectProfileCards = Array.from({ length: 10 }, (_, index) => <Card_Profile_Project key={index} />);
+  const handleSubmit = async () => {
+    const token = localStorage.getItem("token");
+    const payload = {
+      memberId,
+      name,
+      introduce,
+      job,
+      specificDuty: selectedSubJob,
+      link,
+      technics: selectedStacks,
+    };
+    try {
+      await axios.post(`${API_BASE_URL}/information/edit`, payload, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      // 수정 완료 후 reload
+      console.log("Profile edit successful");
+      window.location.reload();
+    } catch (error) {
+      console.error("Error editing user profile:", error);
+    }
+  };
+
+  const isFormFilled = introduce && job && selectedSubJob && selectedStacks.length > 0;
+
+  const jobOptions = {
+    프론트엔드: ["IOS", "안드로이드", "웹프론트엔드", "웹퍼블리셔"],
+    백엔드: ["웹서버", "블록체인", "AI", "DB/빅데이터/DS", "게임 서버"],
+    디자인: ["그래픽 디자인", "프로덕트 디자인", "3D 디자인", "모션/이펙트디자인"],
+    기획: ["기획"],
+  };
+
+  const [subJobs, setSubJobs] = useState([]);
+
+  function SelectedStacksDisplay({ selectedStacks }) {
+    const [showTooltip, setShowTooltip] = useState(false);
+
+    const handleMouseEnter = () => setShowTooltip(true);
+
+    const handleMouseLeave = () => setShowTooltip(false);
+
+    const displayText =
+      selectedStacks.length > 3 ? (
+        <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          {selectedStacks.slice(0, 3).join(", ")} +{selectedStacks.length - 3}
+        </span>
+      ) : (
+        selectedStacks.join(", ")
+      );
+
+    return (
+      <div
+        style={{
+          color: "grey",
+          fontSize: "14px",
+          textAlign: "left",
+          marginTop: "10px",
+          position: "relative",
+        }}
+      >
+        {selectedStacks.length > 0 ? displayText : "아직 선택한 스택이 없어요."}
+        {showTooltip && selectedStacks.length > 3 && (
+          <div
+            style={{
+              position: "absolute",
+              top: "20px",
+              right: "130px",
+              backgroundColor: "white",
+              border: "1px solid #ccc",
+              padding: "5px",
+              borderRadius: "5px",
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              gap: "5px",
+              width: "130px",
+              zIndex: 1000,
+            }}
+          >
+            {selectedStacks.slice(3).map((stack, index) => (
+              <span key={index} style={{ flex: "1 0 calc(50% - 5px)", textAlign: "center" }}>
+                {stack}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  const toggleStackSelection = () => {
+    setShowStackSelection(!showStackSelection);
+  };
+
+  const handleStackSelect = (stack) => {
+    if (!selectedStacks.includes(stack)) {
+      setSelectedStacks([...selectedStacks, stack]);
+    } else {
+      setSelectedStacks(selectedStacks.filter((s) => s !== stack));
+    }
+  };
+
+  const handleJobSelect = (selectedJob) => {
+    setJob(selectedJob);
+    setSubJobs(jobOptions[selectedJob] || []);
+    setSelectedSubJob("");
+    setIsJobDropdownOpen(false);
+    setIsSubJobDropdownOpen(false);
+  };
+
+  const handleSubJobSelect = (subJob) => {
+    setSelectedSubJob(subJob);
+    setIsSubJobDropdownOpen(false);
+  };
+
+  const toggleJobDropdown = () => setIsJobDropdownOpen(!isJobDropdownOpen);
+  const toggleSubJobDropdown = () => setIsSubJobDropdownOpen(!isSubJobDropdownOpen);
+
+  const stackOptions = ["JavaScript", "TypeScript", "React", "Vue", "Nodejs", "Spring", "Java", "Nextjs", "Nestjs", "Express", "Go", "C", "Python", "Django", "Swift", "Kotlin", "MySQL", "MongoDB", "php", "GraphQL", "Firebase", "ReactNative", "Unity", "Flutter", "AWS", "Kubernetes", "Docker", "Git", "Figma", "Zeplin"];
+
+  // const projectProfileCards = Array.from({ length: 10 }, (_, index) => <Card_Profile_Project key={index} />);
+  const projectProfileCards = projects.map((project) => <Card_Profile_Project key={project.bno} projectName={project.projectName} description={project.description} category={project.category} boardLike={project.boardLike} onClick={() => handleToProjectLink(project.bno)} />);
+
+  const handleToProjectLink = (bno) => {
+    navigate(`/project/read/${bno}`);
+  };
 
   return (
     <div className="page">
@@ -43,8 +217,115 @@ function MyPage() {
                 </div>
               </div>
 
+              <div className="Neright-section">
+                <div className="Neinput-container" style={{ marginTop: "10px", marginBottom: "0" }}>
+                  <div className="Neinput-container" style={{ marginTop: "30px" }}>
+                    <p style={{ marginBottom: "0px" }}>
+                      이름<span className="Nerequired"> *필수</span>
+                    </p>
+                    <input className="Neunderline-input" style={{ width: "245px" }} type="text" placeholder="이름을 입력해주세요" value={name} readOnly />
+                  </div>
+
+                  <div className="Neinput-container" style={{ marginTop: "30px" }}>
+                    <p style={{ marginBottom: "0px" }}>한줄 소개</p>
+                    <input className="Neunderline-input" style={{ width: "245px" }} type="text" placeholder="한줄 소개를 입력해주세요" value={introduce} onChange={(e) => setIntroduce(e.target.value)} />
+                  </div>
+
+                  <p className="Nepassword-label" style={{ marginTop: "30px" }}>
+                    (희망) 직무 선택
+                  </p>
+                  <div className="Neflex-container" style={{ display: "flex", justifyContent: "space-between", width: "260px" }}>
+                    <div className="input-container dropdown-container job-dropdown">
+                      <button className={`job-select-btn ${job ? "selected" : ""}`} style={{ width: "100%" }} onClick={toggleJobDropdown}>
+                        {job || "직무 선택"}
+                        {isJobDropdownOpen ? <IoChevronUpOutline className="dropdown-arrow" /> : <IoChevronDownOutline className="dropdown-arrow" />}
+                      </button>
+                      {isJobDropdownOpen && (
+                        <ul className="dropdown-list">
+                          {Object.keys(jobOptions).map((jobOption) => (
+                            <li key={jobOption} onClick={() => handleJobSelect(jobOption)} className={`dropdown-item ${job === jobOption ? "selected" : ""}`}>
+                              {jobOption}
+                              <span className={`checkmark-circle ${job === jobOption ? "selected" : ""}`}></span>
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
+
+                    <div className="input-container dropdown-container ">
+                      <button className={`job-select-btn ${selectedSubJob ? "selected" : ""}`} style={{ width: "100%" }} onClick={toggleSubJobDropdown}>
+                        {selectedSubJob || "세부직무 선택"}
+                        {isSubJobDropdownOpen ? <IoChevronUpOutline className="dropdown-arrow" /> : <IoChevronDownOutline className="dropdown-arrow" />}
+                      </button>
+                      {isSubJobDropdownOpen && (
+                        <ul className="dropdown-list" style={{ width: "140%" }}>
+                          {subJobs.map((subJob) => (
+                            <li key={subJob} onClick={() => handleSubJobSelect(subJob)} className={`dropdown-item ${selectedSubJob === subJob ? "selected" : ""}`}>
+                              {subJob}
+                              <span className={`checkmark-circle ${selectedSubJob === subJob ? "selected" : ""}`}></span>
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="Neinput-container" style={{ marginTop: "0", width: "245px", height: "50px" }}>
+                  <div className="Neflex-container" style={{ alignItems: "center", justifyContent: "space-between", display: "flex", width: "100%" }}>
+                    <p className="Nepassword-label" style={{ marginBottom: 0, marginRight: "auto" }}>
+                      사용 가능한 스택
+                    </p>
+                    <span onClick={toggleStackSelection} style={{ cursor: "pointer", color: "gray", display: "inline-flex", alignItems: "center" }}>
+                      스택 선택하기 <IoChevronForward />
+                    </span>
+                  </div>
+
+                  <div className="stack-section">
+                    {showStackSelection && (
+                      <div className="stack-dropdown-container">
+                        <div className="stack-dropdown-header">스택 선택하기</div>
+                        <div className="stack-dropdown-description">사용 가능한 스택을 모두 골라 주세요!</div>
+                        <div className="stack-option-list" style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+                          {stackOptions.map((stack) => (
+                            <div
+                              key={stack}
+                              onClick={() => handleStackSelect(stack)}
+                              className={`stack-option ${selectedStacks.includes(stack) ? "selected" : ""}`}
+                              style={{
+                                padding: "8px 20px",
+                                border: `1px solid ${selectedStacks.includes(stack) ? "rgba(67, 154, 255, 1)" : "#ccc"}`,
+                                borderRadius: "20px",
+                                cursor: "pointer",
+                                color: selectedStacks.includes(stack) ? "rgba(67, 154, 255, 1)" : "#000",
+                                backgroundColor: selectedStacks.includes(stack) ? "#fff" : "#fff",
+                                display: "inline-flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                textAlign: "center",
+                              }}
+                            >
+                              {stack}
+                            </div>
+                          ))}
+                        </div>
+                        <button onClick={() => setShowStackSelection(false)} className="complete-button">
+                          완료
+                        </button>
+                      </div>
+                    )}
+                    <SelectedStacksDisplay selectedStacks={selectedStacks} />
+                  </div>
+                </div>
+
+                <div className="Neinput-container" style={{ marginTop: "10px" }}>
+                  <p style={{ marginBottom: "0px" }}>Link</p>
+                  <input className="Neunderline-input" style={{ width: "245px" }} type="text" placeholder="github.com/" value={link} onChange={(e) => setLink(e.target.value)} />
+                </div>
+              </div>
+
               <div className="Profile-profile-space">
-                <div className="Profile-profile-modify-btn">
+                <div className={`Profile-profile-modify-btn ${isFormFilled ? "active" : ""}`} onClick={handleSubmit} disabled={!isFormFilled}>
                   <p>수정하기</p>
                 </div>
               </div>
